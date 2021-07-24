67222616551622f3c5e700ed94d41b8b9b446869
Philip Mann <mannphilip7@gmail.com>
2021-07-23 19:23:38 -0500

    updated inventory.js with more products

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -2,7 +2,7 @@ module.exports = {
     
     "inventory": [
         {
-            "item": "Ghost Face Killer",
+            "item": "What's your favorite scary movie?",
             "item_type": "Earrings",
             "price": "15.00",
             "quantity": "3",
@@ -39,6 +39,66 @@ module.exports = {
             "id": "004",
             "image": "https://i.ibb.co/SNCGgTR/IMG-4646.jpg",
             "description": "Strawberry Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Bow Rains",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "2",
+            "id": "005",
+            "image": "https://i.ibb.co/LYMs4q9/IMG-4652.jpg",
+            "description": "Blue and Yellow Rainbow Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Bow Rains",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "5",
+            "id": "006",
+            "image": "https://i.ibb.co/2Z38Mnf/IMG-4647.jpg",
+            "description": "Purple, pink, and green rainbow Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Watermelon Pom-poms",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "1",
+            "id": "007",
+            "image": "https://i.ibb.co/MZBkM5q/IMG-4654.jpg",
+            "description": "Watermelon Pom-pom Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Blue Pom-poms",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "3",
+            "id": "008",
+            "image": "https://i.ibb.co/DgcXg17/IMG-4645.jpg",
+            "description": "Blue Pom-pom Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Flowers",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "3",
+            "id": "009",
+            "image": "https://i.ibb.co/vkjpt6Y/IMG-4649.jpg",
+            "description": "Flower Earrings",
+            "size": "" 
+        },
+        {
+            "item": "Crescent Moons",
+            "item_type": "Earrings",
+            "price": "15.00",
+            "quantity": "3",
+            "id": "009",
+            "image": "https://i.ibb.co/R9yVRp3/IMG-4648.jpg",
+            "description": "Moon Earrings",
             "size": "" 
         }
     ]
