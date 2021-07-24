d97a0caf84109e704a0f82cd82244adfc1b2e300
Philip Mann <mannphilip7@gmail.com>
2021-07-23 22:53:00 -0500

    added image to landing page with text

--- react-ui/src/components/landing.jsx
+++ react-ui/src/components/landing.jsx
@@ -3,8 +3,31 @@ import React from 'react';
 export default function Landing() {
 
     return (
-        <div className="gl card-container d-flex mt-3 justify-content-center">
-            <h1>Ghoul Latte</h1>
+        <div className="gl landing-container d-flex mt-3 justify-content-center">
+            <div className="landing-img">
+                <img src="https://i.ibb.co/YR6x638/IMG-4656.jpg" alt="cat" />
+            </div>
+            <div className="landing-text">
+                <p>
+                    Sit and stare throwup on your pillow,
+                    or chew on cable. Bathe private parts 
+                    with tongue then lick owner's face why 
+                    must they do that walk on keyboard plop 
+                    down in the middle where everybody walks 
+                    ask to go outside and ask to come inside 
+                    and ask to go outside and ask to come inside, 
+                    get suspicious of own shadow then go play with
+                    toilette paper. Immediately regret falling into 
+                    bathtub claws in the eye of the beholder poop on 
+                    floor and watch human clean up. Experiences short
+                    bursts of poo-phoria after going to the loo leave
+                    hair everywhere sleep good morning sunshine so 
+                    climb leg, yet open the door, let me out, let me 
+                    out, let me-out, let me-aow, let meaow, meaow!
+                    or thinking longingly about tuna brine. Kitty 
+                    friends are not food so attack feet.
+                </p>
+            </div>
         </div>
     );
 }
