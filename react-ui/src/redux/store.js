0fdaaf5234e2035847c9d5f2369538bb96b081f6
Matthew Day <matt@matthewday.net>
2021-08-03 14:24:06 -0400

    add cart to the store

--- react-ui/src/redux/store.js
+++ react-ui/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { productsApi } from "./services/api";
 import { emailSlice } from './slice/email';
+import { cartSlice } from './slice/cart';
 
 export const store = configureStore({
     reducer: {
       [productsApi.reducerPath]: productsApi.reducer,
-      email: emailSlice.reducer
+      email: emailSlice.reducer,
+      cart: cartSlice.reducer
     },
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
