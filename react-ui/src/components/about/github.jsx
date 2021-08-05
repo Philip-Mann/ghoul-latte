709cf85673eb64153fb108010cb2517e271c6761
Philip Mann <mannphilip7@gmail.com>
2021-08-05 17:05:45 -0500

    added target _blank to github link

--- react-ui/src/components/about/github.jsx
+++ react-ui/src/components/about/github.jsx
@@ -1,6 +1,6 @@
 const Github = () => {
 
-    return <a href="https://github.com/Philip-Mann/ghoul-latte"><h1>GitHub</h1></a>
+    return <a href="https://github.com/Philip-Mann/ghoul-latte" target="_blank" rel="noreferrer"><h1>GitHub</h1></a>
 }
 
 export default Github;
